a636269a6cb4a5b69d5fd91b8ec9d52e4671e4fb
Alexander Sipakov <alexsipacov@gmail.com>
2026-04-05 13:16:47 +0300

    created first chart, minor changes in the norm. code

---
+++ EDA.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+custom_palette = {
+    "Розетка": "#05bc52",
+    "OLX": "#23e5db", 
+    "Фокстрот": "#e95d2a",
+    "Епіцентр К": "#1060c1",
+    "Prom.ua": "#8b08fa",
+}
+
+def set_custom_style():
+    sns.set_theme(style="white", palette="viridis")
+    sns.set_context("notebook", font_scale=1.1)
+    
+    plt.rcParams['axes.spines.top'] = False
+    plt.rcParams['axes.spines.right'] = False
+    
+    plt.rcParams['figure.dpi'] = 100
+    plt.rcParams['figure.figsize'] = (10, 6)
+
+def initial_analysis():
+    csv_path = "datasets/big-5-scaled.csv"
+    
+    df = pd.read_csv(csv_path)
+
+    sns.set_theme(style="whitegrid")
+
+    x_axis_label = "Час (роки та місяці)"
+    y_axis_label = "Відносна популярність запитів"
+    season_label = "Сезонний всплеск запитів (листоп.-груд.)"
+    chart_title = "Порівняння популярності українських онлайн-магазинів"
+
+    # 4. Melt the data
+    # Seaborn works best when data is "Long Format" 
+    # (One column for 'Store Name', one for 'Value')
+    df_long = df.melt(id_vars=["Time"], value_vars=df.columns[3:], 
+                      var_name="Retailer", value_name="Scaled Value")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.lineplot(data=df_long, x="Time", y="Scaled Value", hue="Retailer", ax=ax, palette=custom_palette)
+
+    # Draw the seasonal surge in demand
+    years = pd.to_datetime(df['Time']).dt.year.unique()
+    
+    data_start = pd.to_datetime(df['Time']).min()
+    data_end = pd.to_datetime(df['Time']).max()
+
+    for year in years:
+        s_start = pd.to_datetime(f"{year}-11-01")
+        s_end = pd.to_datetime(f"{year}-12-31")
+        
+        if s_start <= data_end and s_end >= data_start:
+            actual_end = min(s_end, data_end)
+            
+            ax.axvspan(s_start.strftime('%Y-%m'), 
+                    actual_end.strftime('%Y-%m'), 
+                    color='gray', alpha=0.2, 
+                    label=season_label if year == years[0] else "")
+
+    # Custom X- and Y- axis
+
+    ticks_pos = list(np.arange(0.0, 1.01, 0.1)) 
+    tick_labels = [f"{p*100:.0f}%" for p in ticks_pos]
+
+    plt.xticks(ticks=df['Time'][::12], rotation=45)
+    plt.yticks(ticks=ticks_pos, labels=tick_labels)
+
+    plt.xlabel(x_axis_label)
+    plt.ylabel(y_axis_label)
+
+    ax.vlines(x=np.arange(0, 270, 3), ymin=0, ymax=1, 
+          colors='lightgray', linestyle=':', linewidth=1)
+
+
+    plt.title(chart_title, loc='left', fontsize=16)
+    plt.legend(loc='upper right')
+    plt.tight_layout()
+    plt.show()
+
+initial_analysis()
--- data-cleanup.py
+++ data-cleanup.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 def clean_data():
     try:
@@ -7,7 +10,7 @@ def clean_data():
         precise_files_folder = './datasets/precision-enhancement-ds'
         output_file = './datasets/big-5-scaled.csv'
 
-        all_scaled_dfs = []
+        scaled_dfs = []
 
         df_main = pd.read_csv(main_csv_path, index_col='Time', parse_dates=True) # Combined data for the websites
 
@@ -30,15 +33,24 @@ def clean_data():
                     # Compress the full range of df_precise to range [0; peak_value] and replace original.
                     df_precise[column_name] = df_precise[column_name] * (peak_value / 100)
                     
-                    all_scaled_dfs.append(df_precise)
+                    scaled_dfs.append(df_precise)
                     print(f"Scaled {column_name} to {peak_value}")
             else:
                 print(f"Skipping {filename}: '{column_name}' not in Main CSV.")
-        if all_scaled_dfs:
-            df_merged = pd.concat(all_scaled_dfs, axis=1) # Merge based on the 'Time' index
-            df_merged = df_merged / 100
-            
-            df_merged.to_csv(output_file)
+        if scaled_dfs:
+            df = pd.concat(scaled_dfs, axis=1) # Merge based on the 'Time' index
+
+            df = df / 100
+
+            df.index = pd.to_datetime(df.index).to_period('M')
+            df["Year"]  = df.index.year
+            df["Month"] = df.index.month
+
+            cols_to_front = ['Year', 'Month']
+            other_cols = [c for c in df.columns if c not in cols_to_front]
+            df = df[cols_to_front + other_cols]
+
+            df.to_csv(output_file)
             print(f"All files processed and saved to {output_file} successfully.")
         else:
             print("No matching files were found to process.")
